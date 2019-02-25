9f9f8b40bd642f6a373f71de0fb255a991b9b795
Vedvardhan <vedvardhan.g@gmail.com>
2019-02-25 10:27:18 +0530

    updated

---
+++ js.js
@@ -0,0 +1,14 @@
+/*function play() {
+  if (document.getElementById('r').clicked) {
+    var r = 2;
+    var p = 3;
+    var s = 1;
+
+    var computerPlay = math.random(r, p, s)
+    console.log(computerPlay)
+
+  } ;
+
+   document.getElementById('p');
+   document.getElementById('s');
+}*/
